d50aee26936bb64d2a9d877792b60369cea3a702
RodrigoNdeC <rodrigondec@gmail.com>
2017-08-26 20:59:54 -0300

    minor fix

--- app/mod_core/models.py
+++ app/mod_core/models.py
@@ -58,6 +58,7 @@ class Usuario(db.Model):
     __tablename__ = 'usuario'
     id_usuario = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(80))
+    data_nascimento = db.Column(db.Date)
     path_foto = db.Column(db.String(80))
     email = db.Column(db.String(50), unique=True)
     senha = db.Column(db.String(64))
@@ -71,11 +72,6 @@ class Usuario(db.Model):
     agendas_colaborando = db.relationship("Agenda",
                             secondary=agenda_colaborador,
                             back_populates="colaboradores")
-
-
-    tipo = db.Column(db.String(30))
-    __mapper_args__ = {'polymorphic_identity': __tablename__,
-                       'polymorphic_on': tipo}
 
 
 class Pais(db.Model):
