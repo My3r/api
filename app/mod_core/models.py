25040a6646ef0dfcf7f44dd46b2d93269ccbd162
RodrigoNdeC <rodrigondec@gmail.com>
2017-08-26 12:06:13 -0300

    pais, estado e cidade ok

--- app/mod_core/models.py
+++ app/mod_core/models.py
@@ -46,18 +46,37 @@ class Usuario(db.Model):
     __mapper_args__ = {'polymorphic_identity': __tablename__,
                        'polymorphic_on': tipo}
 
-    def __init__(self, nome, email, senha):
-        self.nome = nome
-        self.email = email
-        self.senha = senha
+
+class Pais(db.Model):
+    __tablename__ = 'pais'
+    id_pais = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(80))
+    sigla = db.Column(db.String(4))
+
+    estados = db.relationship("Estado", back_populates="pais")
 
 
-class Administrador(Usuario):
-    __tablename__ = 'administrador'
-    id_usuario = db.Column(db.Integer(), db.ForeignKey("usuario.id_usuario", ondelete="CASCADE"), primary_key=True)
+class Estado(db.Model):
+    __tablename__ = 'estado'
+    id_estado = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(80))
+    uf = db.Column(db.String(4))
 
-    client_id = db.Column(db.Integer, db.ForeignKey('client.id_client'))
-    client = db.relationship("Client", uselist=False, back_populates='administrador')
+    pais_id = db.Column(db.Integer, db.ForeignKey('pais.id_pais'))
+    pais = db.relationship("Pais", back_populates="estados")
+
+    cidades = db.relationship("Cidade", back_populates="estado")
+
+
+class Cidade(db.Model):
+    __tablename__ = 'cidade'
+    id_pais = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(80))
+    sigla = db.Column(db.String(5))
+
+    estado_id = db.Column(db.Integer, db.ForeignKey('estado.id_estado'))
+    estado = db.relationship("Estado", back_populates="cidades")
+
 
     __mapper_args__ = {'polymorphic_identity': __tablename__}
 
