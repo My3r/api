b7d1b100a5b5950fea834c332c334c6dae495551
RodrigoNdeC <rodrigondec@gmail.com>
2017-08-27 14:43:38 -0300

    algoritmo de busca de locais por interesse

--- app/mod_local/controllers.py
+++ app/mod_local/controllers.py
@@ -1,9 +1,9 @@
-from flask import request, Blueprint
-from flask_restplus import Resource, Namespace, fields, abort
+from flask import Blueprint
+from flask_restplus import Resource, Namespace, fields
 
 from app import db
-from app.mod_core.models import Usuario, Local, Tag, Interacao
-from app.utils import abort_if_none, fill_object, msg
+from app.mod_core.models import Usuario, Local, Tag, Cidade
+from app.utils import abort_if_none, msg
 
 # Define the blueprint: 'auth', set its url prefix: app.url/core
 mod_local = Blueprint('local', __name__, url_prefix='/local')
@@ -17,7 +17,10 @@ local_m = ns.model('local', {
     'endereço': fields.String,
     'path_foto': fields.String,
     'lat': fields.Float,
-    'lng': fields.Float
+    'lng': fields.Float,
+    'instagram_1': fields.String,
+    'instagram_2': fields.String,
+    'instagram_3': fields.String
 })
 
 tag_m = ns.model('tag', {
@@ -77,3 +80,35 @@ class LocalTagController(Resource):
         local.tags.remove(tag)
         db.session.commit()
         return msg('Sucesso!')
+
+
+@ns.route('/cidade/<int:id_cidade>/usuario/<int:id_usuario>')
+@ns.response(404, 'Não encontrado')
+class LocalInteresseController(Resource):
+    @ns.marshal_with(local_m)
+    @ns.response(200, 'Lista de locais ordenado decrescente por interesse do usuário')
+    def get(self, id_cidade, id_usuario):
+        """Retorna lista de locais ordenado por quantidade de tags de interesse do usuário"""
+        usuario = Usuario.query.filter_by(id_usuario=id_usuario).first()
+        abort_if_none(usuario, 404, 'Não achado')
+        cidade = Cidade.query.filter_by(id_cidade=id_cidade).first()
+        abort_if_none(cidade, 404, 'Não achado')
+
+        locais_interacoes = []
+        for interacao in usuario.interacoes:
+            locais_interacoes.append(Local.query.filter_by(id_local=interacao.local_id).first())
+
+        locais_disponiveis = [local for local in cidade.locais if local not in locais_interacoes]
+
+        tags_complementares = []
+        for i in range(0, len(locais_disponiveis)):
+            tags_complementares.append([tag for tag in locais_disponiveis[i].tags if tag not in usuario.interesses])
+            print(locais_disponiveis[i])
+
+        for i in range(0, len(locais_disponiveis)):
+            locais_disponiveis[i].tags = [tag for tag in locais_disponiveis[i].tags if tag not in tags_complementares[i]]
+
+        locais_disponiveis = sorted(locais_disponiveis, key=lambda local: len(local.tags), reverse=True)
+
+        return locais_disponiveis
+
