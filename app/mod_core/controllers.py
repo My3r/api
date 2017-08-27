63152c944f209ea008a52deeabedeb67d96d0d2f
RodrigoNdeC <rodrigondec@gmail.com>
2017-08-27 01:25:27 -0300

    cruds terminados

--- app/mod_core/controllers.py
+++ app/mod_core/controllers.py
@@ -5,7 +5,7 @@ from werkzeug.security import check_password_hash
 import sys
 
 from app import db
-from app.mod_core.models import Usuario
+from app.mod_core.models import Usuario, Local, Tag
 from app.utils import abort_if_none, fill_object, msg
 
 # Define the blueprint: 'auth', set its url prefix: app.url/core
@@ -27,15 +27,42 @@ user_m_expect = ns.model('usario', {
     'senha': fields.String
 })
 
+local_m = ns.model('local', {
+    'id_local': fields.Integer,
+    'nome': fields.String,
+    'descricao': fields.String,
+    'endereço': fields.String,
+    'path_foto': fields.String,
+    'lat': fields.Float,
+    'lng': fields.Float
+})
+local_m_expect = ns.model('local', {
+    'nome': fields.String,
+    'descricao': fields.String,
+    'endereço': fields.String,
+    'path_foto': fields.String,
+    'lat': fields.Float,
+    'lng': fields.Float
+})
+
+tag_m = ns.model('tag', {
+    'id_tag': fields.Integer,
+    'nome': fields.String
+})
+tag_m_expect = ns.model('tag', {
+    'nome': fields.String
+})
+
+
 @ns.route('/usuario/<int:id>')
 @ns.response(403, 'Usuário não tem permissão')
 @ns.response(400, 'ID não é inteiro')
 @ns.response(404, 'Não encontrado')
-class UserController(Resource):
+class UsuarioController(Resource):
     @ns.marshal_with(user_m)
-    @ns.response(200, 'Returns the user model on the body of the response')
+    @ns.response(200, 'Retorna o model usuario no corpo da resposta')
     def get(self, id):
-        """Get an user by ID"""
+        """Retorna um usuário pelo ID"""
         us = Usuario.query \
             .filter(Usuario.id_usuario == id) \
             .first()
@@ -45,7 +72,7 @@ class UserController(Resource):
     @ns.response(200, 'Usuario atualizado')
     @ns.expect(user_m_expect)
     def put(self, id):
-        """Update an user by ID"""
+        """Atualiza um usuário pelo ID"""
         us = Usuario.query\
             .filter(Usuario.id_usuario == id)\
             .first()
@@ -56,28 +83,15 @@ class UserController(Resource):
 
         return msg('success!')
 
-    @ns.response(200, 'User disabled on db')
-    def delete(self, id, ):
-        """Delete an user by ID"""
-        us = Usuario.query.filter(Usario.id_usuario == id) \
-            .first()
-        abort_if_none(us, 404, 'not found')
-
-
-
-        db.session.commit()
-
-        return msg('disabled on db')
-
 
 @ns.route('/usuario/')
 @ns.response(403, 'Usuário não tem permissão')
-class UserPostController(Resource):
+class UsuarioPostController(Resource):
     @ns.response(400, 'Um dos argumentos está mal formado')
     @ns.response(200, 'Retorna uma lista de usuarios com o critério passado', user_m)
     @ns.marshal_with(user_m)
     def get(self):
-        """Get a list of users"""
+        """Retorna uma lista de usuários"""
         return Usuario.query.all()
 
     @ns.response(404, 'Erro inesperado')
@@ -85,7 +99,7 @@ class UserPostController(Resource):
     @ns.response(200, 'Usuario inserido')
     @ns.expect(user_m_expect)
     def post(self):
-        """Create a new user"""
+        """Cria um novo usuário"""
         us = Usuario()
         fill_object(us, request.json)
         db.session.add(us)
@@ -97,28 +111,113 @@ class UserPostController(Resource):
         return msg(us.id_usuario, 'id')
 
 
-# @ns.route('/user/resetpassword/')
-# @ns.response(403, 'User is not logged, not have permission or the password is incorrect')
-# @ns.response(400, 'The input is wrong')
-# @ns.response(404, 'User not Found')
-# @ns.response(200, 'The password is successfully altered. Obs: You need login again, to receive new token.')
-# @ns.header('Authorization', 'The authorization token')
-# class PasswordController(Resource):
-#
-#     @ns.expect(password_reset_m)
-#     def put(self):
-#         """Change the password"""
-#         us = User.query \
-#             .filter(User.disabled == 0) \
-#             .filter(User.id_user == g.current_user) \
-#             .first()
-#         abort_if_none(us, 404, 'User not found')
-#
-#         if not check_password_hash(us.password, request.json['old_password']):
-#             return msg('Old password incorrect'), 403
-#
-#         us.password = request.json['password']
-#         db.session.commit()
-#         cache.blacklisted_tokens.append(request.headers['Authorization'])
-#
-#         return msg('success!')
+@ns.route('/local/<int:id>')
+@ns.response(403, 'Usuário não tem permissão')
+@ns.response(400, 'ID não é inteiro')
+@ns.response(404, 'Não encontrado')
+class LocalController(Resource):
+    @ns.marshal_with(local_m)
+    @ns.response(200, 'Retorna o model local no corpo da resposta')
+    def get(self, id):
+        """Retorna um local pelo ID"""
+        lc = Local.query \
+            .filter(Local.id_local == id) \
+            .first()
+        abort_if_none(lc, 404, 'Não achado')
+        return lc
+
+    @ns.response(200, 'Local atualizado')
+    @ns.expect(local_m_expect)
+    def put(self, id):
+        """Atualiza um local pelo ID"""
+        lc = Local.query \
+            .filter(Local.id_local == id) \
+            .first()
+        abort_if_none(lc, 404, 'Não achado')
+
+        fill_object(lc, request.json)
+        db.session.commit()
+
+        return msg('success!')
+
+@ns.route('/local/')
+@ns.response(403, 'Usuário não tem permissão')
+class LocalPostController(Resource):
+    @ns.response(400, 'Um dos argumentos está mal formado')
+    @ns.response(200, 'Retorna uma lista de locais com o critério passado', local_m)
+    @ns.marshal_with(local_m)
+    def get(self):
+        """Retorna uma lista locais"""
+        return Local.query.all()
+
+    @ns.response(404, 'Erro inesperado')
+    @ns.response(400, 'O modelo está mal formado')
+    @ns.response(200, 'Local inserido')
+    @ns.expect(local_m_expect)
+    def post(self):
+        """Cria um novo local"""
+        lc = Local()
+        fill_object(lc, request.json)
+        db.session.add(lc)
+        try:
+            db.session.commit()
+        except Exception as e:
+            abort(404, e.__str__())
+
+        return msg(lc.id_local, 'id')
+
+
+@ns.route('/tag/<int:id>')
+@ns.response(403, 'Usuário não tem permissão')
+@ns.response(400, 'ID não é inteiro')
+@ns.response(404, 'Não encontrado')
+class TagController(Resource):
+    @ns.marshal_with(tag_m)
+    @ns.response(200, 'Retorna o model tag no corpo da resposta')
+    def get(self, id):
+        """Retorna uma tag pelo ID"""
+        tg = Tag.query \
+            .filter(Tag.id_tag == id) \
+            .first()
+        abort_if_none(tg, 404, 'Não achado')
+        return tg
+
+    @ns.response(200, 'Tag atualizada')
+    @ns.expect(tag_m_expect)
+    def put(self, id):
+        """Atualiza uma tag pelo ID"""
+        tg = Tag.query \
+            .filter(Tag.id_tag == id) \
+            .first()
+        abort_if_none(tg, 404, 'Não achado')
+
+        fill_object(tg, request.json)
+        db.session.commit()
+
+        return msg('success!')
+
+@ns.route('/tag/')
+@ns.response(403, 'Usuário não tem permissão')
+class TagPostController(Resource):
+    @ns.response(400, 'Um dos argumentos está mal formado')
+    @ns.response(200, 'Retorna uma lista de locais com o critério passado', tag_m)
+    @ns.marshal_with(tag_m)
+    def get(self):
+        """Retorna uma lista tags"""
+        return Tag.query.all()
+
+    @ns.response(404, 'Erro inesperado')
+    @ns.response(400, 'O modelo está mal formado')
+    @ns.response(200, 'Tag inserido')
+    @ns.expect(tag_m_expect)
+    def post(self):
+        """Cria uma novo tag"""
+        tg = Tag()
+        fill_object(tg, request.json)
+        db.session.add(tg)
+        try:
+            db.session.commit()
+        except Exception as e:
+            abort(404, e.__str__())
+
+        return msg(tg.id_tag, 'id')
