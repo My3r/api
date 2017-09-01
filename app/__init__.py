71188506302910745d1fc457b4b18925b80ce06b
RodrigoNdeC <rodrigondec@gmail.com>
2017-08-31 23:09:11 -0300

    jwt e auth adicionados

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_restplus import Api
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 import logging
 from logging.handlers import RotatingFileHandler
-import pymysql
 
-pymysql.install_as_MySQLdb()
 
 # Define the WSGI application object
 app = Flask(__name__)
+app.secret_key = 'super-secret'
+jwt = JWTManager(app)
+app.config['JWT_HEADER_TYPE'] = ''
 
 # Define Api application object
 api = Api(app, version='0.5a', title='My3r',
@@ -49,6 +51,7 @@ from app.mod_core import models
 
 
 # Import a module / component using its blueprint handler variable (mod_auth)
+from app.mod_auth.controllers import mod_auth as auth_module, ns as ns_auth
 from app.mod_core.controllers import mod_core as core_module, ns as ns_core
 from app.mod_interacao.controllers import mod_interacao as interacao_module, ns as ns_interacao
 from app.mod_interesse.controllers import mod_interesse as interesse_module, ns as ns_interesse
@@ -58,12 +61,14 @@ from app.mod_local.controllers import mod_local as local_module, ns as ns_local
 # from app.mod_events.controllers import mod_event as event_module, ns as ns_event
 
 # Register blueprint(s)
+app.register_blueprint(auth_module)
 app.register_blueprint(core_module)
 app.register_blueprint(interacao_module)
 app.register_blueprint(interesse_module)
 app.register_blueprint(local_module)
 
 # Register API namespace(s)
+api.add_namespace(ns_auth)
 api.add_namespace(ns_core)
 api.add_namespace(ns_interacao)
 api.add_namespace(ns_interesse)
